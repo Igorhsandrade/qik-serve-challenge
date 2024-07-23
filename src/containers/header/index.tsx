6a0b193521566362301b92ef6e5e832902450dd5
igorhsandrade <igorhsandrade@gmail.com>
2024-07-23 12:31:54 -0300

    header finished

--- src/containers/header/index.tsx
+++ src/containers/header/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import MobileNav from '../../components/mobileNav';
 import Nav from '../../components/nav';
+import styles from './styles.module.css';
 
 const Header = () => {
   const location = useLocation();
@@ -15,9 +16,12 @@ const Header = () => {
   }, [location]);
 
   return (
-    <div>
+    <div className={styles.header}>
       <MobileNav selectedMenuItem={selectedMenuItem} />
       <Nav selectedMenuItem={selectedMenuItem} />
+      <div className={styles.headerImageContainer}>
+        <img src="https://preodemo.gumlet.io/usr/venue/7602/web/646fbf3abf9d0.png" />
+      </div>
     </div>
   );
 };
